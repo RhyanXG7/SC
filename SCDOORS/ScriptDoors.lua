01ae3beab263145ef3e524f6a2bc167b6211a956
RhyanXG7 <143280548+RhyanXG7@users.noreply.github.com>
2024-08-27 18:48:30 -0300

    Update ScriptDoors.lua

--- SCDOORS/ScriptDoors.lua
+++ SCDOORS/ScriptDoors.lua
@@ -1,5 +1,5 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({IntroText = "Doors GUI v1.2",Name = "Doors", HidePremium = false, SaveConfig = true, ConfigFolder = "DoorsSex"})
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/RhyanXG7/SC/BetterStar/SCDOORS/original_paste.lua')))()
+local Window = OrionLib:MakeWindow({IntroText = "RHSEEKER V1.0.0",Name = "Doors", HidePremium = false, SaveConfig = true, ConfigFolder = "DoorsSex"})
 if game.PlaceId == 6516141723 then
     OrionLib:MakeNotification({
         Name = "Error",
